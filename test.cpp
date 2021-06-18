eb6af8acc4c0dde4f5f12562953ccd8e70523e68
DESKTOP-RT1LEL3\seung gyu <wa975@naver.com>
2021-06-18 19:22:42 +0900

    commit a

--- test.cpp
+++ test.cpp
@@ -6,176 +6,195 @@
 #include<stdlib.h>
 #include<stralign.h>
 #include <malloc.h>
+#include <process.h>
 
-
-#define pause system("pause > nul")    //그냥 내가 많이 쓰는 커맨드를 줄여준 것이다.
+#define pause system("pause > nul")    
 #define cls system("cls") 
+
+#define KEY_DOWN(vk_code) ((GetAsyncKeyState(vk_code & 0x8000)?1:0))
+#define KEY_UP(vk_code) ((GetAsyncKeyState(vk_code & 0x8000)?0:1))
+
+
+int isEnd = 0;
+int score = 0;
+int state = 0;
+int heart = 3;
 
 void SetColor(int color);
 void mainPtr(void);
 void gotoxy(int x, int y);
 void CursorView(char show);
-void Teacher_close();
-void Teacher_open();
+void view_score();
+
+
+
+
+
+unsigned __stdcall Thread_A(void* arg)
+{
+    while (1) {
+
+        if ((GetAsyncKeyState(VK_SPACE) & 0x8000) && state == 1) {
+            isEnd = 1;
+            break;
+        }
+        else if ((GetAsyncKeyState(VK_SPACE) & 0x8000)) { // || ((GetAsyncKeyState(VK_SPACE) & 0x8001))) { 
+            gotoxy(90, 8); printf("score = %d ", score);
+            score++;
+            Sleep(100);
+        }
+        
+    }
+    return 0;
+}
+
+
+unsigned __stdcall Thread_B(void* arg)
+{
+    while (1) {
+        //system("cls");
+        int a = 0;
+        int b = 0;
+        a = rand() % 5 + 2; // 난수 생성(2~6초까지)
+        b = 600 * a;
+        gotoxy(50, 4); printf("ㅡ    ㅡ\n");
+        state = 0;
+        Sleep(b);
+
+
+        //system("cls");
+        int c = 0;
+        int d = 0;
+        c = rand() % 5 + 2; // 난수 생성(2~6초까지)
+        d = 600 * a;
+        gotoxy(50, 4); printf("o      o\n");
+        //Sleep(100);
+        state = 1;
+        Sleep(d);
+    }
+    return 0;
+}
+
+//unsigned _stdcall Thread_C(void* arg)
+
+
+
 
 int main(void)
 {
-	int score = 1;
-	int POS = 4;
-	CursorView(0);    //커서의 깜빡임을 숨겨준다.
-	system("COLOR 0F");    //화면 배경을 검정, 글씨 색깔을 하얀색으로 설정해 준다.
-	mainPtr();
-	while (true) {
-		if (GetAsyncKeyState(VK_LEFT))
-			if (POS == 0) POS = 3;
-			else POS -= 1;
-		else if (GetAsyncKeyState(VK_RIGHT))
-			if (POS == 3) POS = 0;
-			else POS += 1;
-		else if (GetAsyncKeyState(VK_RETURN))//엔터를 눌렀을 때
-			break;
-		switch (POS) {
-		case 0:
-			SetColor(11);
-			gotoxy(10, 4); printf("새로  하기");
-			SetColor(15);
-			gotoxy(35, 4); printf("이어  하기");
-			gotoxy(60, 4); printf("랭킹  보기");
-			gotoxy(85, 4); printf("게임  종료");
-			break;
-		case 1:
-			gotoxy(10, 4); printf("새로  하기");
-			SetColor(11);
-			gotoxy(35, 4); printf("이어  하기");
-			SetColor(15);
-			gotoxy(60, 4); printf("랭킹  보기");
-			gotoxy(85, 4); printf("게임  종료");
-			break;
-		case 2:
-			gotoxy(10, 4); printf("새로  하기");
-			gotoxy(35, 4); printf("이어  하기");
-			SetColor(11);
-			gotoxy(60, 4); printf("랭킹  보기");
-			SetColor(15);
-			gotoxy(85, 4); printf("게임  종료");
-			break;
-		case 3:
-			gotoxy(10, 4); printf("새로  하기");
-			gotoxy(35, 4); printf("이어  하기");
-			gotoxy(60, 4); printf("랭킹  보기");
-			SetColor(11);
-			gotoxy(85, 4); printf("게임  종료");
-			SetColor(15);
-			break;
-		default: break;
-		}
-		Sleep(100);
-	}
-	cls;
-	if (POS == 0) puts("새로 하기를 눌렀습니다.");
-	else if (POS == 1) puts("이어 하기를 눌렀습니다.");
-	else if (POS == 2) puts("랭킹 보기를 눌렀습니다.");
+
+    int POS = 0;
+    int cnt = 0;
+    CursorView(0);    //커서의 깜빡임을 숨겨준다.
+    system("COLOR 0F");    //화면 배경을 검정, 글씨 색깔을 하얀색으로 설정해 준다.
+    mainPtr();
+    while (true) {
+        if (GetAsyncKeyState(VK_LEFT))
+            if (POS == 0) POS = 1;
+            else POS -= 1;
+        else if (GetAsyncKeyState(VK_RIGHT))
+            if (POS == 1) POS = 0;
+            else POS += 1;
+        else if (GetAsyncKeyState(VK_RETURN))//엔터를 눌렀을 때
+            break;
+        switch (POS) {
+        case 0:
+            SetColor(11);
+            gotoxy(25, 4); printf("새로  하기");
+            SetColor(15);
+            gotoxy(70, 4); printf("게임  종료");
+            break;
+        case 1:
+            gotoxy(25, 4); printf("새로  하기");
+            SetColor(11);
+            gotoxy(70, 4); printf("게임  종료");
+            SetColor(15);
+            break;
+        default: break;
+        }
+        Sleep(100);
+    }
+    cls;
+    if (POS == 0) puts("새로 하기를 눌렀습니다.");
+    cls;
+
+    HANDLE th1 = (HANDLE)_beginthreadex(NULL, 0, Thread_A, 0, 0, NULL);
+    HANDLE th2 = (HANDLE)_beginthreadex(NULL, 0, Thread_B, 0, 0, NULL);
+    //_beginthreadex(NULL, 0, Thread_C, 0, 0, NULL);
 
 
-	while(1)
-	{
-		Teacher_close();
-			if (_kbhit())
-			{
-				if (_getch() == ' ')
-				score = score + 1;
-				gotoxy(90, 8); printf("score = %d", score);
-			}
+    int count = 60;
+    while (1) {
 
-		Teacher_open();
-		if (_kbhit())
-		{
-			if (_getch() == ' ')
-				break;
+        //gotoxy(15, 8); printf("%d", heart);
 
-		}
-		
-			
-	}
-	return 0;
+        if (count >= 10) {
+            gotoxy(10, 8); printf("%d", count);
+            count = count - 1;
+            Sleep(1000);
+        }
+        if (count < 10) {
+            cls;
+            gotoxy(11, 8); printf("%d", count);
+            count = count - 1;
+            Sleep(1000);
+        }
+        if (count == 0)
+        {
+            break;
+        }
+        if (isEnd) {
+            break;
+        }
+    }
+
+    printf("\n최종스코어는 : %d", score);
+
+    return 0;
 }
 
 
 
-void view_score() //스페이스바 한번당 1점 출력하는 함수
-{
 
-}
-
-void view_time() //60초 제한시간 출력하는 함수
-{
-
-}
-
-
-void Teacher_close()
-{
-		//system("cls");
-		int a = 0;
-		int b = 0;
-		a = rand() % 5 + 2; // 난수 생성(2~6초까지)
-		b = 600 * a;
-		gotoxy(50, 4); printf("ㅡ    ㅡ\n");
-		Sleep(b);
-}
-
-
-void Teacher_open()
-{
-	//system("cls");
-	int a = 0;
-	int b = 0;
-	a = rand() % 5 + 2; // 난수 생성(2~6초까지)
-	b = 600 * a;
-	gotoxy(50, 4); printf("o      o\n");
-	Sleep(b);
-}
 
 
 void mainPtr(void) {
-	system("mode con: cols=106 lines=9");   //참고로 cols 2단위가 특수문자 또는 한글 1글자다.
-	system("title 선생님 몰래 춤추기");
-	puts("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
-	puts("■                                                                                                      ■");
-	puts("■                                                                                                      ■");
-	puts("■                                                                                                      ■");
-	puts("■                                                                                                      ■");
-	puts("■                                                                                                      ■");
-	puts("■                                                                                                      ■");
-	puts("■                                                                                                      ■");
-	printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
-	gotoxy(10, 4); printf("새로  하기");
-	gotoxy(35, 4); printf("이어  하기");
-	gotoxy(60, 4); printf("랭킹  보기");
-	gotoxy(85, 4); printf("게임  종료");
-	return;
+    system("mode con: cols=106 lines=9");   //참고로 cols 2단위가 특수문자 또는 한글 1글자다.
+    system("title 선생님 몰래 춤추기");
+    puts("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+    puts("■                                                                                                      ■");
+    puts("■                                                                                                      ■");
+    puts("■                                                                                                      ■");
+    puts("■                                                                                                      ■");
+    puts("■                                                                                                      ■");
+    puts("■                                                                                                      ■");
+    puts("■                                                                                                      ■");
+    printf("■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■■");
+    gotoxy(25, 4); printf("새로  하기");
+    gotoxy(70, 4); printf("게임  종료");
+    return;
 }
 
 void gotoxy(int x, int y) {
 
-	COORD pos = { x, y };
-	SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
+    COORD pos = { x, y };
+    SetConsoleCursorPosition(GetStdHandle(STD_OUTPUT_HANDLE), pos);
 }
 
 void CursorView(char show)
 {
-	HANDLE hConsole;
-	CONSOLE_CURSOR_INFO ConsoleCursor;
+    HANDLE hConsole;
+    CONSOLE_CURSOR_INFO ConsoleCursor;
 
-	hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
 
-	ConsoleCursor.bVisible = show;
-	ConsoleCursor.dwSize = 1;
+    ConsoleCursor.bVisible = show;
+    ConsoleCursor.dwSize = 1;
 
-	SetConsoleCursorInfo(hConsole, &ConsoleCursor);
+    SetConsoleCursorInfo(hConsole, &ConsoleCursor);
 }
 
 void SetColor(int color)
 {
-	SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
+    SetConsoleTextAttribute(GetStdHandle(STD_OUTPUT_HANDLE), color);
 }
